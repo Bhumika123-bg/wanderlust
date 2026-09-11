4331c9f0ce4f92f5f44184b4003ad34aab044972
Bhumika123-bg <gbhumika690@gmail.com>
2026-09-11 22:54:46 +0530

    Add mongoose schema for Listing model

--- init/data.js
+++ init/data.js
@@ -1,1 +1,35 @@
+const mongoose = require("mongoose");
 
+const listingSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String
+    },
+
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85"
+    },
+
+    price: {
+        type: Number
+    },
+
+    location: {
+        type: String
+    },
+
+    country: {
+        type: String
+    }
+
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
